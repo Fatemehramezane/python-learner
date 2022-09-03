39526c48f2bfbe62bb62882e7f729e8aed8e7719
Fatemeh Ramezani <arghavan6253@gmail.com>
2022-09-03 11:31:43 -0700

    aroor

--- hello'name and age.py
+++ hello'name and age.py
@@ -1,11 +1,16 @@
+# start program with Hello
 print("hi")
-name=input("Enter your name : ")
-name=("name")
-print("hello,name")
+
+name=input("Enter your" name : )
+print("hello",name)
+
 yourbirthyear =input("Enter yourbirthyear :")
 yourbirthyear=int(yourbirthyear)
+
 thisyear=input("Enter thisyear:  ")
 thisyear=int(thisyear)
+
 age=thisyear-yourbirthyear
-age=int(age)
-print("your age is:    ,name")
+
+
+print("your age is:",age    ,name)
