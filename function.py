f4398bc7432358e5f34d343cfdd02f1ad390d03e
Fatemeh Ramezani <arghavan6253@gmail.com>
2022-09-03 05:30:44 -0700

    new change

---
+++ function.py
@@ -0,0 +1,6 @@
+Housofwork=input("Enter your Housofwork")
+Housofwork=int(Housofwork)
+Hourlyfee=input("Enter your Hourlyfee")
+Hourlyfee=int(Hourlyfee)
+Function=Housofwork*Hourlyfee
+print("your Function is    ")
