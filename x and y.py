6e89716b909856e8804ddd51f61bf5a89f6bdad7
Fatemeh Ramezani <arghavan6253@gmail.com>
2022-09-04 09:27:25 -0700

    9 lines

--- x and y.py
+++ x and y.py
@@ -1,5 +1,9 @@
-x=input("Enter a number for x :")
-x=int(x)
-y=input(2*x-2)
-y=int(y)
+# A number for X :
+X = input ( "Enter a number for" X )
+X = int ( X )  
 
+# Computing Y :
+Y = 2 * X - 2
+
+# Output :
+print ( "Answer is:" Y )
