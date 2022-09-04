4ab1e326f048c2ead64d65f125e79e3cad676289
Fatemeh Ramezani <arghavan6253@gmail.com>
2022-09-04 00:10:16 -0700

    Write

---
+++ Triangle Perimeter.py
@@ -0,0 +1,17 @@
+# The size of the first side
+Side1 = input ( "Enter a number for the" Side1 )
+Side1 = int ( Side1 )
+
+# The size of the second side
+Side2 = input ( "Enter a number for the" Side2 )
+Side2 = int ( Side2 ) 
+
+# The size of the third size
+Side3 = input ( "Enter a number for the" Side3 )
+Side3 = int ( Side3 )
+
+# Environment
+Environment = Side1 + Side2 + Side3 
+
+# Output
+print ( "The output is: " Environment)
